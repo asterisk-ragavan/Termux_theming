97d6cd816a9dd3a56b6c68473b6249b64fa1d1ca
Sakthi Ragavan <75389172+asterisk-ragavan@users.noreply.github.com>
2022-01-26 17:10:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,7 +36,9 @@ cd /data/data/com.termux/files/usr/etc
 
 echo 'clear
 figlet -f standard S H E E R I O
-echo "welcome back Mr sakith ragavan your system is ready now"' >> zshrc
+echo "welcome back Mr sakith ragavan your system is ready now"
+alias c = 'clear'
+alias q = 'exit'' >> zshrc
 cd ~
 echo 'prompt_context() {
   # Custom (Random emoji)
