fb3de2cc0152785bc1c343e69efd1c70505a35a5
Sakthi Ragavan <75389172+asterisk-ragavan@users.noreply.github.com>
2022-02-01 22:31:24 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,61 @@
+ apt & apt upgrade
+
+apt install figlet -y
+apt install nano -y
+
+
+pushd /data/data/com.termux/files/usr/etc
+rm -rf motd
+popd
+
+
+#!/data/data/com.termux/files/usr/bin/bash
+termux-setup-storage
+apt install -y git zsh
+git clone https://github.com/Cabbagec/termux-ohmyzsh.git "$HOME/termux-ohmyzsh" --depth 1
+
+
+mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp -R "$HOME/termux-ohmyzsh/.termux" "$HOME/.termux"
+
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
+sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+
+chsh -s zsh
+
+
+echo "oh-my-zsh install complete!\nChoose your color scheme now~"
+$HOME/.termux/colors.sh
+
+
+echo "Choose your font now~"
+$HOME/.termux/fonts.sh
+cd /data/data/com.termux/files/usr/etc
+
+
+echo 'clear
+figlet -f standard S H E E R I O
+echo "welcome back Mr sakith ragavan your system is ready now"' >> zshrc
+cd ~
+echo 'prompt_context() {
+  # Custom (Random emoji)
+  emojis=("⚡️" "🔥" "💀" "👑" "😎" "🐸" "🐵" "🦄" "🌈" "🍻" "🚀" "💡" "🎉" "🔑" "🇹🇭" "🚦" "🌙")
+  RAND_EMOJI_N=$(( $RANDOM % ${#emojis[@]} + 1))
+  prompt_segment black default "${emojis[$RAND_EMOJI_N]} "
+}' >> .zshrc
+echo "warning this script can run only once runing this again may cause problems!"
+echo " "
+echo "Please restart the Termux app...."
+mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> $HOME/.termux/termux.properties
+exit
